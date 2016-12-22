159b31c00e52277ff775d5b65afff0fa0b44eab3
Philipp Anders <philippanders@Philipps-MacBook-Pro.local>
2016-12-22 19:29:06 +0100

    two main functions: one for simple clustering, one for elbow method

--- src/main/java/de/automatic/ui/colorconfiguraiton/main/Main.java
+++ src/main/java/de/automatic/ui/colorconfiguraiton/main/Main.java
@@ -25,50 +25,60 @@ import de.automatic.ui.colorconfiguraiton.vis.ThreeDimHistogramVisualizer;
 
 public class Main {
 
-	static int k = 4;
-	static int maxK = 20;
-	static int attempts = 5;
+	static int k = 5;
+	static int maxK = 15;
+	static int attempts = 3;
 	static String file = "resources/kanye_small.jpg";
 
 	public static void main(String[] args) {
 
-		ArrayList<Pair<Double, Double>> list = new ArrayList<Pair<Double, Double>>();
-
-		for (int k = 1; k <= maxK; k++) {
-			double accError = 0;
-			for (int a = 0; a < attempts; a++) {
-				Histogram histogram = null;
-				try {
-					histogram = (new ImageReader(new File(file))).getHistogram();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				AbstractKmeans clusterer = new KmeansPlusPlus(k);
-				ClusterContainer clusters = clusterer.clusterToEnd(histogram);
-				accError += clusters.getError();
-			}
-			double meanError = accError / (double) attempts;
-			System.out.println(meanError);
-			list.add(new Pair<Double, Double>((double) k, meanError));
-			// System.out.println("Number of Steps: " +
-			// clusterer.getStepCount());
+		Histogram histogram = null;
+		try {
+			histogram = (new ImageReader(new File(file))).getHistogram();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
-		// new
-		// PaletteShower(ClusterListConversionService.convertToHashSet(clusters),
-		// "K-Means").visualizePalette();
+		AbstractKmeans clusterer = new KmeansPlusPlus(k);
+		ClusterContainer clusters = clusterer.clusterToEnd(histogram);
+		new PaletteShower(ClusterListConversionService.convertToHashSet(clusters), "K-Means").visualizePalette();
 
 		// new OneDimHistogramVisualizer("Channel Histograms", histogram,
 		// clusters);
 
-		// try {
-		// new ThreeDimHistogramVisualizer(histogram, clusters);
-		// } catch (Exception e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+		try {
+			new ThreeDimHistogramVisualizer(histogram, clusters);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		new GraphVisualizer(list);
 	}
+	
+//	public static void main(String[] args) {
+//
+//		ArrayList<Pair<Double, Double>> list = new ArrayList<Pair<Double, Double>>();
+//		Histogram histogram = null;
+//		try {
+//			histogram = (new ImageReader(new File(file))).getHistogram();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		for (int k = 1; k <= maxK; k++) {
+//			double accError = 0;
+//			for (int a = 0; a < attempts; a++) {
+//
+//				AbstractKmeans clusterer = new KmeansPlusPlus(k);
+//				ClusterContainer clusters = clusterer.clusterToEnd(histogram);
+//				accError += clusters.getError();
+//			}
+//			double meanError = accError / (double) attempts;
+//			list.add(new Pair<Double, Double>((double) k, meanError));
+//
+//		}
+//
+//		new GraphVisualizer(list);
+//	}
 
 }
