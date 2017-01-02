167e03d1944092cff8b034a37e52065a67c15e1b
Philipp Anders <philipp.anders.pa@gmail.com>
2017-01-02 22:45:45 +0100

    improved seed filter

--- src/main/java/de/automatic/ui/colorconfiguraiton/services/SeedFilterer.java
+++ src/main/java/de/automatic/ui/colorconfiguraiton/services/SeedFilterer.java
@@ -7,7 +7,7 @@ import de.automatic.ui.colorconfiguraiton.entities.SampleList;
 
 public class SeedFilterer {
 
-	private static final double threshold = 0.25;
+	private static final double threshold = 0.2;
 
 	public static void filterSeeds(SampleList seeds) {
 		Collections.shuffle(seeds);
@@ -18,9 +18,15 @@ public class SeedFilterer {
 				Sample s1 = seeds.get(i);
 				for (int j = i + 1; j < seeds.size(); j++) {
 					Sample s2 = seeds.get(j);
-					if (ErrorCalculationService.getEucledianDistance(s1, s2) < threshold) {
+					if (ErrorCalculationService.getEucledianDistance(ConversionService.toHsi(s1),
+							ConversionService.toHsi(s2)) < threshold) {
 						System.out.println("removed seed " + s2);
-						seeds.remove(s2);
+						if (accDistance(s1, seeds) < accDistance(s2, seeds)) {
+							seeds.remove(s1);
+						} else {
+							seeds.remove(s2);
+						}
+
 						removedSeed = true;
 						break outer;
 					}
@@ -30,6 +36,14 @@ public class SeedFilterer {
 			}
 		}
 
+	}
+
+	private static double accDistance(Sample s, SampleList samples) {
+		double distance = 0;
+		for (Sample s2 : samples) {
+			distance += ErrorCalculationService.getEucledianDistance(s, s2);
+		}
+		return distance;
 	}
 
 }
