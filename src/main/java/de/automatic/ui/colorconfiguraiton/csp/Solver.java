71d4c1bd5219f6c3a67d8064b5a5af0f4fd29035
Philipp Anders <philipp.anders.pa@gmail.com>
2017-03-21 17:55:42 +0100

    - Example of Factor Graph Usage: Get Accent Color with Highest Saturation

--- src/main/java/de/automatic/ui/colorconfiguraiton/csp/Solver.java
+++ src/main/java/de/automatic/ui/colorconfiguraiton/csp/Solver.java
@@ -18,32 +18,34 @@ import de.automatic.ui.colorconfiguraiton.visualisation.ColorVarShower;
 		public static class AccentColorFactor extends FactorFunction {
 			@Override
 			public final double evalEnergy(Value[] args) {
-				int state1 = (int) args[0].getObject();
-				int state2 = (int) args[1].getObject();
+//				int state1 = (int) args[0].getObject();
+//				int state2 = (int) args[1].getObject();
+//
+//				int diff = state1 - state2;
+//
+//				return diff >= 2 ? 0 : Double.POSITIVE_INFINITY;
+				
+				ColorVar state1 = (ColorVar) args[0].getObject();
 
-				int diff = state1 - state2;
-
-				return diff >= 2 ? 0 : Double.POSITIVE_INFINITY;
+				return -Math.log(state1.getS());
 			}
 		}
 
 		public static void solve(ArrayList<ColorVar> vars) {
 			
-			new ColorVarShower(vars.get(1));
+//			new ColorVarShower(vars.get(1));
 			
 			FactorGraph graph = new FactorGraph();
 			
 			ColorVar[] domain = new ColorVar[vars.size()];
 			vars.toArray(domain);
-//			System.out.println(domain[0].getH());
 
-//			DiscreteDomain domain = DiscreteDomain.create(1, 2, 3);
-//			Discrete accent = new Discrete(domain);
-//			graph.addFactor(new AccentColorFactor());
-//
-//			graph.solve();
-//
-//			System.out.println(x.getValue());
+			Discrete accent = new Discrete(domain);
+			graph.addFactor(new AccentColorFactor(), accent);
+
+			graph.solve();
+
+			new ColorVarShower((ColorVar) accent.getValue());
 //			System.out.println(y.getValue());
 
 		}
